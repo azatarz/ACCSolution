be5d5182ee566b5bf155fa22560627479d4960ac
houssamdamien <Houssamdamien@live.com>
2021-06-30 18:57:32 +0300

    Add project files.

---
+++ ACCSolution.Entities/Models/Databases/RepositoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACCSolution.Entities.Models.Databases
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // DBSETS
+
+
+
+    }
+}
